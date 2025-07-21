e436fdacac33021fc2ffc8f8e5f68fa5d9a743b0
Radosław Michalski <radoslaw.michalski@lhsystems.com>
2025-07-21 18:55:25 +0200

    HIPSUM-1 repo app init commit

---
+++ src/main/java/com/bv/repositoryapp/RepositoryappApplication.java
@@ -0,0 +1,13 @@
+package com.bv.repositoryapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepositoryappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RepositoryappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bv/repositoryapp/RepositoryappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bv.repositoryapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepositoryappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
